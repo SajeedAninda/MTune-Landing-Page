1fff52bf47057d5d7ca65007d39f5af66e79751c
SajeedAninda <sajaninda@gmail.com>
2024-10-07 21:45:31 +0600

    Added sidebar in navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,58 @@
-import React from 'react';
-import logo from "../assets/logo.png"
+import React, { useState } from 'react';
+import logo from "../assets/logo.png";
 import { FaFacebook } from "react-icons/fa";
 import { HiOutlineMenu } from 'react-icons/hi';
-
+import { IoClose } from 'react-icons/io5'; // For close icon
 
 const Navbar = () => {
-    return (
-        <div className='h-[15vh] md:h-[13vh] bg-gradient-to-r from-white to-[#d3132e] shadow-md flex items-center'>
-            <div className='w-[90%] md:w-[85%] mx-auto flex justify-between items-center'>
-                <div className='flex gap-2 items-center'>
-                    <div className='block md:hidden'>
-                        <HiOutlineMenu className='text-[35px] text-[#252b46]'/>
-                    </div>
-                    <img className='w-[80px]' src={logo} alt="" />
-                </div>
+    const [sidebarOpen, setSidebarOpen] = useState(false);
 
-                <div>
-                    <ul className='text-[18px] font-medium text-white flex gap-10 items-center'>
-                        <div className='hidden md:flex gap-10 items-center'>
-                            <li className='hover:opacity-70 cursor-pointer transition-opacity delay-75'>Features</li>
-                            <li className='hover:opacity-70 cursor-pointer transition-opacity delay-75'>Pricing</li>
-                            <li className='hover:opacity-70 cursor-pointer transition-opacity delay-75'>Contact</li>
+    const toggleSidebar = () => {
+        setSidebarOpen(!sidebarOpen);
+    };
+
+    return (
+        <div className='relative'>
+            <div className='h-[15vh] lg:h-[13vh] bg-gradient-to-r from-white to-[#d3132e] shadow-md flex items-center'>
+                <div className='w-[90%] md:w-[85%] mx-auto flex justify-between items-center'>
+                    <div className='flex gap-2 items-center'>
+                        <div className='block md:hidden'>
+                            <HiOutlineMenu className='text-[35px] text-[#252b46]' onClick={toggleSidebar} />
                         </div>
-                        <a href='https://www.facebook.com/mt.callertune' target='_blank' className='px-3 md:px-4 py-2 md:py-3 rounded-lg bg-transparent border-2 border-white text-white hover:bg-white hover:text-[#d3132e] font-semibold transition-colors delay-75 flex gap-2 items-center '>
-                            <FaFacebook className='text-[22px]' />
-                            MTune
-                        </a>
-                    </ul>
+                        <img className='w-[80px]' src={logo} alt="MTune Logo" />
+                    </div>
+
+                    <div>
+                        <ul className='text-[18px] font-medium text-white flex gap-10 items-center'>
+                            <div className='hidden md:flex gap-10 items-center'>
+                                <li className='hover:opacity-70 cursor-pointer transition-opacity delay-75'>Features</li>
+                                <li className='hover:opacity-70 cursor-pointer transition-opacity delay-75'>Pricing</li>
+                                <li className='hover:opacity-70 cursor-pointer transition-opacity delay-75'>Contact</li>
+                            </div>
+                            <a href='https://www.facebook.com/mt.callertune' target='_blank' className='px-3 md:px-4 py-2 md:py-3 rounded-lg bg-transparent border-2 border-white text-white hover:bg-white hover:text-[#d3132e] font-semibold transition-colors delay-75 flex gap-2 items-center '>
+                                <FaFacebook className='text-[22px]' />
+                                MTune
+                            </a>
+                        </ul>
+                    </div>
                 </div>
             </div>
+
+            <div className={`md:hidden fixed top-0 left-0 h-full w-[70%] sm:w-[40%] bg-white z-50 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out shadow-lg`}>
+                <div className='flex justify-between items-center p-4'>
+                    <img className='w-[60px]' src={logo} alt="MTune Logo" />
+                    <IoClose className='text-[30px] text-[#252b46] cursor-pointer' onClick={toggleSidebar} />
+                </div>
+                <ul className='mt-10 flex flex-col gap-6 text-lg text-[#252b46] px-6'>
+                    <li className='hover:text-[#d3132e] cursor-pointer transition-colors'>Features</li>
+                    <li className='hover:text-[#d3132e] cursor-pointer transition-colors'>Pricing</li>
+                    <li className='hover:text-[#d3132e] cursor-pointer transition-colors'>Contact</li>
+                </ul>
+            </div>
+
+            {sidebarOpen && (
+                <div className="md:hidden fixed inset-0 bg-black opacity-50 z-40" onClick={toggleSidebar}></div>
+            )}
         </div>
     );
 };
