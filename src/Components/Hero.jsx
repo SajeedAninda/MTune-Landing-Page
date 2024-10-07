e015040fe929f550109d141942000f5fa04a0b6a
SajeedAninda <sajaninda@gmail.com>
2024-10-07 18:41:57 +0600

    Made hero section responsive

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,19 +9,19 @@ import { RiWhatsappFill } from "react-icons/ri";
 
 const Hero = () => {
     return (
-        <div className='h-[87vh] w-[80%] mx-auto flex justify-between items-center'>
+        <div className='h-fit lg:h-[87vh] w-[80%] mx-auto flex flex-col lg:flex-row justify-between items-center pt-10 lg:pt-0'>
             <div className='flex-1'>
-                <h1 className='text-[#d3132e] text-[100px] font-bold'>
+                <h1 className='text-[#d3132e] text-[60px] md:text-[80px] lg:text-[100px] font-bold text-center lg:text-left'>
                     MTune
                 </h1>
-                <h3 className='text-[#252b46] text-[40px] font-semibold italic'>
+                <h3 className='text-[#252b46] text-[40px] font-semibold italic text-center lg:text-left'>
                     Tune Up your Business
                 </h3>
-                <p className='text-[#252b46] mt-2 font-semibold text-[16px]'>
+                <p className='text-[#252b46] mt-2 font-semibold text-[16px] text-center lg:text-left'>
                     MTune offers custom caller tune services for your brand, along with campaign voiceovers, promotional ads, and creative content to elevate your business image.
                 </p>
 
-                <div className='flex gap-2 mt-5'>
+                <div className='flex justify-center lg:justify-normal gap-2 mt-5'>
                     {/* Call Button */}
                     <a
                         href='tel:+8801575782959'
